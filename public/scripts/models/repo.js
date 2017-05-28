cbce33cb71677a2f31e245fc8a6a12cc837a8ead
christylaguardia <christinelaguardia@gmail.com>
2017-05-28 14:58:04 -0700

    peer progaming

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,14 +7,21 @@
 
   repos.requestRepos = function(callback) {
     // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route. Since the new proxy route is handling our token for us, we do not need it here, which is why we can refactor to to the simpler $.get method.
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    .then(callback);
-  };
+    // $.ajax({
+    //   url: `https://api.github.com/user/repos`,
+    //   type: 'GET',
+    //   headers: {'Authorization': `token ${githubToken}`}
+    // })
+    // .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    // .then(callback);
+    // })
+
+      $.get('/github/user/repos', function(data) {
+        repos.all = data;
+        callback();
+      });
+    }
+
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
